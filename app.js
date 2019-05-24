6da4fa40c723532ff2596977b73ee7f2f493d2c8
AbrahamDN <40493309+AbrahamDN@users.noreply.github.com>
2019-05-24 04:05:05 +0100

    Level Security
    Use npm package " mongoose-encryption " to give the site level 2 security, where passwords are encrypted with the Ceasar Cipher method

---
+++ app.js
@@ -0,0 +1,86 @@
+//jshint esversion:6
+const ejs = require("ejs");
+const bodyPaser = require("body-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyPaser.urlencoded({extended: true}));
+app.use(express.static("Public"));
+app.set("view engine", "ejs");
+//Connect to mongodb
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = "Thisismylittlesecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+/////////////////////////////////////////////////////////////////////////////////
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.route("/login")
+.get(function(req, res){
+  res.render("login");
+})
+
+.post(function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        } else {
+          res.redirect("/login");
+        }
+      }
+    }
+  });
+
+});
+
+
+app.route("/register")
+.get(function(req, res){
+  res.render("register");
+})
+
+.post(function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if (!err) {
+      console.log("New user created");
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//Start server
+app.listen(port, function(err){
+  console.log("Server running on port:" + port);
+});
